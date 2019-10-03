8286b76cb0f946846b2cc8671d9166fa81d065c4
eightseconds <paulseokcho@gmail.com>
2019-10-02 19:50:32 -0700

    Implement quadratic time complexity solution

--- threeNumberSum.js
+++ threeNumberSum.js
@@ -1,7 +1,23 @@
 function threeNumberSum(array, target) {
+    array.sort((a,b) => a - b)
     const triplets = [];
 
     for (let i = 0; i < array.length - 2; i++) {
-        
+        let leftIdx = i + 1
+        let rightIdx = array.length - 1
+        while (leftIdx < rightIdx) {
+            const currentSum = array[i] + array[leftIdx] + array[rightIdx];
+            if (currentSum === target) {
+                triplets.push([array[i], array[leftIdx], array[rightIdx]])
+                leftIdx ++;
+                rightIdx --;
+            } else if (currentSum < target) {
+                leftIdx ++;
+            } else if (currentSum > target) {
+                rightIdx --;
+            }
+        }
     }
+
+    return triplets;
 }
